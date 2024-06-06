3d6abbdd0cd036df053206fbbd24169b445b2759
Your Name <youremail@example.com>
2024-06-06 11:48:16 +0300

    add extra styling, fix redirection, and styling linking

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import axios from "axios";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
 const port = 3000;
-var description;
-app.use(express.static("public"));
+const currentFilePath = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(`${currentFilePath}/public`));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -12,27 +15,51 @@ app.get("/", (req, res) => {
 });
 
 //ANIME POST GET REQUEST
+
+//anime variables
+var anime;
+var titles;
+var images;
+var score;
+var rank;
+var trailer;
+var yearRelease;
+var type;
+var description;
+var filteredTitles;
+//ANIME VARIABLES
+app.get("/random-anime", (req, res) => {
+  res.render("indexAnime.ejs", {
+    trailer: trailer,
+    images: images,
+    titles: filteredTitles,
+    description: description,
+    type: type,
+    score: score,
+    rank: rank,
+    year: yearRelease,
+  });
+  // if there's an error load main page and display the type of error
+});
+
 app.post("/random-anime", async (req, res) => {
   try {
     const result = await axios.get(`https://api.jikan.moe/v4/random/anime`);
-    const anime = result.data.data;
+    anime = result.data.data;
 
-    const titles = anime.titles;
-    const images = anime.images.jpg;
-    const score = {
+    titles = anime.titles;
+    images = anime.images.jpg;
+    score = {
       score: anime.score,
       scoredBy: anime.scored_by,
     };
-    const rank = { rank: anime.rank, popularity: anime.popularity };
-    const trailer = anime.trailer.embed_url;
-    const yearRelease = anime.year;
-    const type = anime.genres;
+    rank = { rank: anime.rank, popularity: anime.popularity };
+    trailer = anime.trailer.embed_url;
+    yearRelease = anime.year;
+    type = anime.genres;
     description = anime.synopsis;
-    //Defaul, Japanese, English
 
-    console.log(yearRelease);
-
-    const filteredTitles = [];
+    filteredTitles = [];
 
     titles.forEach((title) => {
       if (
@@ -57,13 +84,13 @@ app.post("/random-anime", async (req, res) => {
     // if there's an error load main page and display the type of error
   } catch (error) {
     res.render("index.ejs", {
-      error: `This Feature is currently unavailable: ${error.response}`,
+      error: `Generator crashed: ${error.response}`,
     });
   }
 });
 
-app.get("/anime/description", (req, res) => {
-  res.render("anime-description.ejs", { description: description });
+app.get("/anime-description", (req, res) => {
+  res.render("indexAnime-description.ejs", { description: description });
 });
 //ANIME POST GET REQUEST
 
